30899fb2791ad60fe4540b2cc96fa6642b312fa8
Jack DeVries <jdevries3133@gmail.com>
2023-01-21 23:15:57 -0500

    hotfix: remove vitest to restore build health

--- src/test/setup.ts
+++
@@ -1,6 +0,0 @@
-// source: https://markus.oberlehner.net/blog/using-testing-library-jest-dom-with-vitest/
-
-import matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
-
-expect.extend(matchers);
